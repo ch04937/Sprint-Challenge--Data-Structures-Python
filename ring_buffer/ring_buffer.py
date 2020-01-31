b0719d8636690404aac2a91331f2b11c8ed3c7b6
ch04937 <97hernandez.c@gmail.com>
2020-01-31 09:35:56 -0800

    ring append

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -7,15 +7,21 @@ class RingBuffer:
         self.current = None
         self.storage = DoublyLinkedList()
 
+    # adds elements to the buffer
     def append(self, item):
-        pass
+        self.storage.add_to_head(item)
 
+    # returns all of the elements in the buffer in a list in their given order
+    # it should NOT return any None values
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
         # TODO: Your code here
-
+        self.current = self.storage.head
+        print(self.current)
+        # account for max cap
+        if self.capacity == len(self.storage):
+            pass
         return list_buffer_contents
 
 # ----------------Stretch Goal-------------------
@@ -30,3 +36,15 @@ class ArrayRingBuffer:
 
     def get(self):
         pass
+
+
+buffer = RingBuffer(5)
+buffer_2 = RingBuffer(5)
+
+buffer.append('a')
+buffer.append('b')
+buffer.append('c')
+buffer.append('d')
+
+print('\nbuffer length: ', buffer.storage.length)
+print('\nbuffer content: ', buffer.get())
